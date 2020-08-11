199ddf3c2792b03d4360e05852b03f10710b5fc8
Philip <philip.iparaguirre@gmail.com>
2020-08-11 09:58:20 -0700

    backend

---
+++ app.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+let bodyParser = require("body-parser");
+let cors = require("cors");
+let cookeParser = require("cookie-parser");
+
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/votes",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }).then(() => {
+      console.log("DB CONNECTED")
+  }).catch(
+      console.log("OOOPS")
+  )
+
+let authRoutes = require("./routes/auth");
+let userRoutes = require("./routes/user");
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(cookeParser());
+
+app.use("/api",authRoutes);
+app.use("/api",userRoutes);
+
+
+  const PORT = process.env.PORT || 8000;
+
+  if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
+  app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
---
+++ models/user.js
@@ -0,0 +1,81 @@
+let mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v4: uuidv4 } = require('uuid');
+
+var userSchema = new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+      },
+      lastName: {
+        type: String,
+        maxlength: 32,
+        trim: true
+      },
+      email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+      },
+      social: {
+        type: Number,
+        required: true,
+        minlength: 9,
+        maxlength: 9,
+        unique: true
+      },
+      userinfo: {
+        type: String,
+        trim: true
+      },
+      encry_password: {
+        type: String,
+        required: true
+      },
+      salt: String,
+      role: {
+        type: Number,
+        default: 0
+      },
+      vote: {
+        type: Object,
+        default: {}
+      }
+    },
+    { timestamps: true }
+  );
+
+  userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = uuidv4();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function(plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
+
+  securePassword: function(plainpassword) {
+    if (!plainpassword) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
